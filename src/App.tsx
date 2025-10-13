04d0234011a89b5b62729285d0c7ff09442fc26e
Bektur <bekttur@mail.ru>
2025-10-13 11:40:59 +0500

    fix: gh-pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-
-import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import BottomNav from './components/BottomNav/BottomNav';
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <BrowserRouter basename="/REPO">
+    <>
       <Header />
       <main>
         <AppRoutes />
       </main>
       <BottomNav />
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App.tsx';
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Theme>
-      <HashRouter>
+       <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/phaeton-frontend' : '/'}>
         <App />
-      </HashRouter>
+        </BrowserRouter>
     </Theme>
   </StrictMode>
 );
