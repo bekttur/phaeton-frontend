85997ae4b1b53b10b21ccc4621ff9bbdaade53b1
Bektur <bekttur@mail.ru>
2025-10-13 14:51:46 +0500

    changed padding for carousel title

--- src/pages/Home/elements/HeroCarousel.tsx
+++ src/pages/Home/elements/HeroCarousel.tsx
@@ -4,15 +4,15 @@ const HeroCarousel = () => {
   const data = [
     {
       title: 'AI-ассистент запчастей',
-      img: '/images/hero.png',
+      img: 'images/hero.png',
     },
     {
       title: 'AI-ассистент запчастей',
-      img: '/images/hero.png',
+      img: 'images/hero.png',
     },
     {
       title: 'AI-ассистент запчастей',
-      img: '/images/hero.png',
+      img: 'images/hero.png',
     },
   ];
 
@@ -57,12 +57,12 @@ const HeroCarousel = () => {
           data.map((item: any, i: number) => (
             <div
               key={i}
-              className='shrink-0 w-full min-h-[270px] h-auto snap-start font-exo bg-cover bg-center rounded-lg'
+              className='shrink-0 w-full min-h-[270px] h-auto snap-start font-exo bg-cover bg-center bg-no-repeat rounded-lg'
               style={{
                 backgroundImage: `url(${import.meta.env.BASE_URL}${item.img})`,
               }}
             >
-              <div className='w-full h-full flex flex-col items-start justify-between py-5 px-3 rounded-lg'>
+              <div className='w-full h-full flex flex-col items-start justify-between py-1 px-3 rounded-lg'>
                 <p className='text-white text-[20px]/[36px] font-bold capitalize '>
                   {item.title}
                 </p>
