ae22aaec586e1b3650ea101a8810a60a5a3dd121
Bektur <bekttur@mail.ru>
2025-12-25 03:18:59 +0500

    added integration with backend

--- src/context/CityContext.tsx
+++ src/context/CityContext.tsx
@@ -2,14 +2,14 @@
 import { createContext, useContext, useState, type ReactNode,  } from 'react';
 
 interface CityContextType {
-  city: string;
+  city: string | null;
   setCity: (city: string) => void;
 }
 
 const CityContext = createContext<CityContextType | undefined>(undefined);
 
 export const CityProvider = ({ children }: { children: ReactNode }) => {
-  const [city, setCity] = useState('Алматы');
+  const [city, setCity] = useState<string | null>(null);
 
   return (
     <CityContext.Provider value={{ city, setCity }}>
